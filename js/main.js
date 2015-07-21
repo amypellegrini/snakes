cc6ab63826c88b47f00035006e79441685c0d073
Amy Pellegrini <vicesaraco@gmail.com>
2015-07-21 08:43:27 -0300

    First commit!

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function() {
+  var canvas = document.getElementById('canvas');
+
+  var Snake = function() {
+    return {
+      size: 8,
+      pixels: [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+        [6, 0],
+        [7, 0]
+      ]
+    };
+  };
+
+}());
