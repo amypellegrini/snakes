5263294166dc34643769ce170f2feabc0c7ecab1
Amy Pellegrini <vicesaraco@gmail.com>
2015-08-04 15:14:09 -0300

    New modules definition and remove google ad template.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
 (function() {
-  var canvas = document.getElementById('canvas');
+  var
+      canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d');
+
+  var Food = function() {
+    var x,
+        y;
+
+    return {
+      x: x,
+      y: y
+    }
+  };
 
   var Snake = function() {
+    var
+        headPosition = [12, 12],
+        size: 8,
+
+        makeTurn = function() {
+
+        },
+
+        grow = function(n) {
+          // Grow snake pixels size by n pixels,
+        };
+
     return {
-      size: 8,
-      pixels: [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-        [4, 0],
-        [5, 0],
-        [6, 0],
-        [7, 0]
-      ]
+      grow: grow,
+      makeTurn: makeTurn
     };
+  };
+
+  var GamePlay = function() {
+    var
+        start = function() {},
+        pause = function() {};
+
+    return {
+      start: start,
+      pause: pause
+    }
   };
 
 }());
